70545b58bbd3a4605621148062440bf940a419ca
katyy <katrinkatkatrin@yahoo.com>
2023-12-02 11:30:18 +0100

    some improvements

--- task2.py
+++ task2.py
@@ -1,46 +1,35 @@
 def parse_input(user_input):
-    cmd, *args = user_input.split()
+    cmd, *args = user_input.split(maxsplit=2)
     cmd = cmd.strip().lower()
-    return cmd, *args
+    return cmd, args
 
 
-def add_contact(args, contacts):
-    # Спосіб 1
-    # if len(args) != 2:
-    #     return "Please input commmand for add_contacts in next patter\nadd [ім'я] [номер телефону]"
-    # name, phone = args
+def add_or_change_contact(args, contacts):
+    if len(args) != 2:
+        return "Please input the command in the format: add [name] [phone]"
 
-    # Спосіб 2
-    try:
-        name, phone = args
-    except ValueError:
-        return "No response"
-
-    contacts[name] = phone
-    return "Contact added."
-
-
-def change_contact(args, contacts: dict):
     name, phone = args
-    if name not in contacts:
-        return "NO person with such contact"
-
     contacts[name] = phone
-    return "Contact added."
+    return "Contact added/changed."
 
 
 def print_phone_by_name(args, contacts):
+    if not args:
+        return "Please provide a name to search for."
+    
     name = args[0]
     if name not in contacts:
-        return "we cant find such user in our contacts"
+        return f"No contact found for {name}."
+    
     phone = contacts[name]
-    return f"{name=} {phone=}"
+    return f"{name}: {phone}"
 
 
-def print_all_contacts(args, contacts):
-    return "\n".join(
-        f"{name=} {phone=}" for name, phone in contacts.items()
-    )
+def print_all_contacts(contacts):
+    if not contacts:
+        return "No contacts available."
+    
+    return "\n".join(f"{name}: {phone}" for name, phone in contacts.items())
 
 
 def main():
@@ -48,21 +37,19 @@ def main():
     print("Welcome to the assistant bot!")
     while True:
         user_input = input("Enter a command: ")
-        command, *args = parse_input(user_input)
+        command, args = parse_input(user_input)
 
         if command in ["close", "exit"]:
-            print("Good bye!")
+            print("Goodbye!")
             break
         elif command == "hello":
             print("How can I help you?")
-        elif command == "add":
-            print(add_contact(args, contacts))
-        elif command == "change":
-            print(change_contact(args, contacts))
+        elif command == "add" or command == "change":
+            print(add_or_change_contact(args, contacts))
         elif command == "phone":
             print(print_phone_by_name(args, contacts))
         elif command == "all":
-            print(print_all_contacts(args, contacts))
+            print(print_all_contacts(contacts))
         else:
             print("Invalid command.")
 
